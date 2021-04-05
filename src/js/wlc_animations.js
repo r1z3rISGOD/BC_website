cde88ce1d0713ea099e7cb23bfaa4dde67356e02
Kirill <75835582+r1z3rISGOD@users.noreply.github.com>
2021-04-05 20:33:35 +0300

    Update wlc_animations.js
    Changes to position of nickname

--- src/js/wlc_animations.js
+++ src/js/wlc_animations.js
@@ -16,7 +16,7 @@ function descSlideUp(){
 }
 
 function nickSlideOver() {
-    $('.welcome_nick').animate({bottom: '45%', right: '45%', fontSize: '42'}, 1200);
+    $('.welcome_nick').animate({bottom: '46%', right: '48%', fontSize: '42'}, 1200);
 }
 
 function descHide(){
