384854629c7117acbfa670050ef54ab0a97aa76f
r1z3rISGOD <kirikkapchik2@hotmail.com>
2021-04-05 20:23:40 +0300

    first commit

---
+++ src/js/wlc_animations.js
@@ -0,0 +1,37 @@
+welcomeEdit();
+setTimeout(nickSlideUp, 1500);
+setTimeout(descSlideUp, 2700);
+setTimeout(nickSlideOver, 4000);
+setTimeout(descHide, 4000);
+setTimeout(openPage, 4500);
+setTimeout(headerShow, 5000);
+
+
+function nickSlideUp() {
+    $('.welcome_nick').animate({bottom: '+=100'}, 1200);
+}
+
+function descSlideUp(){
+    $('.welcome_desc').animate({opacity: '1'}, 1500);
+}
+
+function nickSlideOver() {
+    $('.welcome_nick').animate({bottom: '+=345', right: '+=920', fontSize: '42'}, 1200);
+}
+
+function descHide(){
+    $('.welcome_desc').slideUp();
+}
+
+function welcomeEdit() {
+   $('.welcome').css({'opacity': '1'});
+}
+
+function openPage() {
+    $('.welcome').animate({opacity: '0'}, 1000);
+    setTimeout (function() {$('.welcome').css({'display': 'none'});}, 700);
+ }
+
+function headerShow() {
+    $('.header').animate({opacity: '1'}, 2000);
+}
